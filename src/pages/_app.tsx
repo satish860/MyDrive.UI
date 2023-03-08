994e483946adf05c04e7fb31e81713ec06ab209a
Satish Venkatakrishnan <satish860@gmail.com>
2023-03-08 10:09:25 +0530

    Login Completed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { themeconst } from "./chakra/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <ChakraProvider theme={themeconst}>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </ChakraProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,25 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { Center, Stack, Text } from "@chakra-ui/react";
 
 export default function Home() {
   const { user, error, isLoading } = useUser();
 
-  return (
-    user ? (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-        <a href="/api/auth/logout">Logout</a>
-      </div>
-    ) :
-    (<a href="/api/auth/login">Login</a>)  
-  )
+  return user ? (
+    <div>
+      <img src={user.picture!} alt={user.name!} />
+      <h2>{user.name}</h2>
+      <p>{user.email}</p>
+      <a href="/api/auth/logout">Logout</a>
+    </div>
+  ) : (
+    <Center height="100vh">
+      <Stack spacing={4} align="center">
+        <Text fontSize="4xl">My Drive</Text>
+        <Text width="70%" align="center">
+          Sign in to get Unlimted storage for your life time.
+        </Text>
+        <a href="/api/auth/login">Login</a>
+      </Stack>
+    </Center>
+  );
 }
